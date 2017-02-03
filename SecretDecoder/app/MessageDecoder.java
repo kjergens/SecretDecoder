2f1510b1e78f90d34ea5d2bdf112fd9e25b0620b
Katie Jergens <katie.jergens@gmail.com>
2017-02-02 22:45:18 -0500

    minor index.html edit

--- SecretDecoder/app/MessageDecoder.java
+++ SecretDecoder/app/MessageDecoder.java
@@ -77,6 +77,8 @@ public class MessageDecoder {
             //
             // 2. Call that function here, e.g. msg = decode(msg);
             //
+            
+            msg = decode(msg);
 
             // Display message
             System.out.println(msg);
@@ -87,5 +89,63 @@ public class MessageDecoder {
 
     }
     
-    // TODO: Here is where you add the decode function.
+    /**
+     * Decodes messages that have been encoded in the following way:
+     * 
+     * 1. All two adjacent vowels were surrounded by v's. e.g. either became
+     * veivther
+     * 
+     * 2. Lowercase vowels shifted over: a became e, e became i, i became o, o
+     * became u, u became a.
+     * 
+     * 3. Every 3-letter combination with an s in the middle was surrounded by f and
+     * r. E.g. "asp" became "faspr"
+     * 
+     * 4. Commas replaced spaces and spaces replaced commas. E.g. "this i know"
+     * became "this,i,know"
+     * 
+     * 5. All "nn" became "Xd".
+     * 
+     * 6. Appended "yy" at end of every word.
+     * 
+     * 
+     * @author YOUR NAME
+     *
+     */
+    public static String decode(String msg) {
+    	String dec = msg;
+    	
+    	// 6. yy
+    	dec = dec.replaceAll("yy\\b", "");
+    	
+    	// 5. nn
+    	dec = dec.replaceAll("Xd", "nn");
+    	
+    	// 4. commas
+    	dec = dec.replaceAll(" ", "COMMA");
+        dec = dec.replaceAll(",", " ");
+        dec = dec.replaceAll("COMMA", ",");
+    	
+    	// 3. faspr
+    	dec = dec.replaceAll("f(\\ws\\w)r", "$1");
+    	
+    	// 2. shift vowels
+    	dec = dec.replaceAll("a", "_U_"); 
+        dec = dec.replaceAll("e", "_A_");
+        dec = dec.replaceAll("i", "_E_");
+        dec = dec.replaceAll("o", "_I_");
+        dec = dec.replaceAll("u", "_O_");
+
+        dec = dec.replaceAll("_E_", "e");
+        dec = dec.replaceAll("_I_", "i");
+        dec = dec.replaceAll("_O_", "o");
+        dec = dec.replaceAll("_U_", "u");
+        dec = dec.replaceAll("_A_", "a");
+        
+        // 1. two vowels together get v on either side, e.g. either -> veivther
+        dec = dec.replaceAll("v([aeiouAEIOU]{2})v", "$1");
+        
+        
+    	return dec;
+    }
 }
